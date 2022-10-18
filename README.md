1f1555913c58493e1f326c943a5684a2a27a64ab
palkovics adam <adam@debian-BULLSEYE-live-builder-AMD64>
2022-10-18 16:24:47 +0200

    get.sh -> sorted output (0.in, 1.in ...)

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ This is my homework which would solve the exercises of [tesztelek2](https://gith
 
 # run
 To use the run script, the repo should be placed in a directory in the root of tesztelek2.
+
+# io
+get.sh should create the desired ios for given input (merging problems/<problem>/io/*.in and *.out into one file)
--- io/get.sh
+++ io/get.sh
@@ -5,6 +5,14 @@ PROBLEMS_PATH="../../problems"
 find $PROBLEMS_PATH/*/io -type d | cut -d '/' -f 4 > problems.txt
 for f in $(cat problems.txt);
 do
-    paste <(cat $PROBLEMS_PATH/$f/io/*.in) <(cat $PROBLEMS_PATH/$f/io/*.out) > $f.io
+    paste <(cat \
+		`ls $PROBLEMS_PATH/$f/io/*.in | sort --field-separator="/" --key=6n` \
+	   ) \
+	  <(cat \
+		`ls $PROBLEMS_PATH/$f/io/*.out | sort --field-separator="/" --key=6n` \
+	   ) \
+	| nl -v 0 \
+	     > $f.io
 done;
 rm problems.txt
+
